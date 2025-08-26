356ab7644dbad1b0eff0e86fd5ea3b5a001c642d
Ujjwal Raj <your-email@example.com>
2025-08-26 15:27:13 +0530

    Second commit

---
+++ script.js
@@ -0,0 +1,60 @@
+var timer = 60;
+var score = 0;
+var hitrn;
+var clickedVal;
+document.querySelector(".elem #highVal").innerHTML =localStorage.getItem("high");
+
+
+function makeBubble() {
+    var clutter = "";
+    for (var i = 1; i <= 108; i++) {
+        clutter = clutter + `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+
+    var timerInt = setInterval(function () {
+
+        if (timer >= 0) {
+            document.querySelector('#timerValue').innerHTML = timer;
+            timer--;
+        }
+        else {
+            clearInterval(timerInt);
+            document.querySelector("#pbtm").innerHTML = `
+            <h2>Score = ${score - 10}</h2>`;     
+            if (localStorage.getItem("high")<(score-10)) {
+                localStorage.setItem("high",score-10);
+            }    
+            document.querySelector(".elem #highVal").innerHTML =localStorage.getItem("high");   
+        }
+
+    }, 1000)
+}
+
+function getNewHit() {
+    hitrn = `${Math.floor(Math.random() * 10)}`;
+    document.querySelector('#hitVal').innerHTML = hitrn;
+}
+
+function increaseScore() {
+    document.querySelector('#scoreVal').textContent = score;
+    score = score + 10;
+}
+document.querySelector('#pbtm').addEventListener('click',function (dets){
+    clickedVal = Number(dets.target.textContent);
+    if(clickedVal == hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+    
+})
+
+
+increaseScore();
+runTimer();
+makeBubble();
+getNewHit();
